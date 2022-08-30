c0eb1496bf309e2ad7edc33f0a58eeeed3627ee8
blurryface2809 <grazimashado@gmail.com>
2022-08-30 15:47:54 -0300

    exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,21 @@
+// # Exercício 1
+
+// Construa um programa que:
+
+// a) Peça ao usuário que insira um número **par**
+
+let numeroPar = Number(prompt(`Digite um número par`))
+
+// b) Imprima no console **o resto da divisão** desse número por 2.
+
+console.log (numeroPar % 2)
+
+// c) Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de código.
+
+// o resto dando zero, testei com 4, 10 e 128.
+
+// d) O que acontece no console se o usuário inserir um número ímpar? Escreva em um comentário de código
+
+// aparece o número 1.
+
+// >💡  **Dica**:   não se esqueça de **converter** as respostas para o tipo número
